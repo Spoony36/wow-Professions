ec4247a28cbb8fc42e2f49c32db28b2a09ff138d
Spoony36 <Spoony36@users.noreply.github.com>
2020-04-18 12:59:54 +0100

    Add files via upload

---
+++ BFA Enchanting.lua
@@ -0,0 +1,69 @@
+local guide = WoWPro:RegisterGuide("BFAEnchanting","Profession","BFAEnchanting", "Spoony", "Neutral")
+WoWPro:GuideIcon(guide,"PRO",333)
+WoWPro:GuideLevels(guide)
+WoWPro:GuideName(guide, "BFAEnchanting")
+WoWPro:GuideSteps(guide, function()
+return [[
+
+
+N Enchanting Trainers:|N| You can find Enchantress Quinni in Dazar'alor at the Terrace of Crafters. You can walk up to a guard in Dazar'alor or Boralus then ask where the Enchanting trainer is. Asking the guard will place a red marker on your map at the trainer's location.
+N Enchanting Trainers:|N| You can find Emily Fairweather is in Boralus at the Tradewinds Market. You can walk up to a guard in Dazar'alor or Boralus then ask where the Enchanting trainer is. Asking the guard will place a red marker on your map at the trainer's location.
+
+
+N Recipe Ranks|N| Most high-level Battle for Azeroth Enchanting recipes has 3 ranks. Higher rank recipes reduce the materials required to create a recipe, and also give skill points much longer. 
+N World Quests|N| Some of the Rank 3 recipes are locked behind World Quests. They show up randomly, so you can't predict when you can get them, but there are new ones up every couple of days. They will be marked with a small enchanting icon on your world map.To unlock World Quest, you must complete the first steps of the War Campaign until you get the quest Uniting Zandalar
+N World Quests|N| Some of the Rank 3 recipes are locked behind World Quests. They show up randomly, so you can't predict when you can get them, but there are new ones up every couple of days. They will be marked with a small enchanting icon on your world map.To unlock World Quest, you must complete the first steps of the War Campaign until you get the questUniting Kul Tiras (Alliance) / 
+N You should put the enchants on an Enchanting Vellum, so you can sell them at the Auction House. Enchanting Vellum is sold by General Goods vendors or Enchanting Supply vendors near your trainer.
+
+N 1 - 30. Make around 40 from any of the glove enchants that cost 5 Gloom Dust.|N| 40 x Any glove enchant - 200 Gloom Dust
+N 30 - 35|N| 7 x Any from the "Seal of..." ring enchants - 84 Gloom Dust. You might have to make a few more. You can also make 5 from any of the .... Hearthing enchants if you can get cheap Umbra Shard.
+
+N Learn the rank 2 ring enchants from the trainer, then get a rank 3 ring enchant recipe
+
+N 35 - 85|N| 70 x Rank 3 ring enchants - 560 Gloom Dust. I recommend waiting until you can get any of the rank 3 ring enchant recipes because the rank 2 is only yellow up to 57, while rank 3 stays yellow up to 75. There is no other good alternative for the 60-85, everything is just really expensive.
+
+N Reaching Revered with the Tortollan Seekers will take some time, but the World Quests can pop up anytime. It's possible you might get one of them right away after you unlock World Quests.
+N Formula: Enchant Ring - Seal of Critical Strike - Sold by Collector Kojo. It requires Tortollan Seekers - Revered.
+N Formula: Enchant Ring - Seal of Versatility - Sold by Collector Kojo. It requires Tortollan Seekers - Revered.
+N Formula: Enchant Ring - Seal of Mastery - Reward from World quest "Work Order: Enchant Ring - Seal of Mastery"
+N Formula: Enchant Ring - Seal of Haste - Reward from World Quest "Work Order: Enchant Ring - Seal of Haste"
+N Alternative recipes|N| If you really want to get to 75 to unlock the best ring enchants, and you don't care about the cost, then you can make these:
+
+N 35-65|N| 40 x Rank 2 ring enchants - 400 Gloom Dust. If you don't have Veiled Crystal, or it's extremely expensive, you should make this one up to 75.
+
+N 65-85|N| 25 x Weapon Enchant - Deadly Navigation - 200 Umbra Shard, 500 Gloom Dust, 25 Veiled Crystal. You can make any other weapon enchants that have the same material cost as this one. Every weapon enchant recipe will be yellow, so 25 is just an approximate number.
+
+N 85 - 95|N| 15 x Weapon Enchant - Deadly Navigation - 120 Umbra Shard, 300 Gloom Dust, 15 Veiled Crystal. You can make any other weapon enchants that have the same material cost as this one. It will be a lot easier to sell at the Auction House if you make a few from each enchants. Every weapon enchants recipe will be yellow, so 15 is just an approximate number.
+
+N Learn the rank 2 weapon enchants from the trainer, then get a rank 3 weapon enchant recipe.
+
+N N 95 - 120|N| 25 x Any rank 3 weapon enchant - 100 Umbra Shard, 625 Gloom Dust, 25 Veiled Crystal
+N Rank 3 Recipes:|N|Formula: Enchant Weapon - Coastal Surge - World Quest "Work Order: Enchant Weapon - Coastal Surge"..Formula: Enchant Weapon - Torrent of Elements - World Quest "Work Order: Enchant Weapon - Torrent of Elements"
+Formula: Enchant Weapon - Gale-Force Striking - Sold by Hoarder Jena (Horde) / Quartermaster Alcorn (Alliance). Requires Revered reputation with Voldunai (Horde) / Order of Embers (Alliance)
+Formula: Enchant Weapon - Siphoning - Sold by Provisioner Lija (Horde) / Provisioner Fray (Alliance). Requires Revered reputation with Talanji's Expedition (Horde) / Proudmoore Admiralty (Alliance)
+The world quest recipes are really easy to get. But they show up randomly, so you can't predict when you can get them.
+
+N 120 - 125|N| 7 x Any Rank 2 "Pact of..." ring enchant - 35 Veiled Crystal, 28 Umbra Shard, 90 Gloom Dust. Rank 2 - Enchanter's Sorcerous Scepter would be cheaper to make, but you can always sell the ring enchants so you will make most of your gold back.
+
+N 125 - 145|N|| 25 x Enchanted Tiki Mask - 500 Gloom Dust, 50 Umbra Shard. The Formula: Enchanted Tiki Mask is dropped by King Rastakhan inside Battle of Dazar'alor raid. It drops in every difficulty so you can get it in Raid Finder. I think it's 100% drop rate. If you don't want to get the Tiki Mask recipe, you can start making the following recipe , but it will be more expensive. 20 x Quick Navigation - 120 Umbra Shard, 360 Gloom Dust, 20 Veiled Crystal
+
+N Visit your trainer Narv at Nazjatar and learn the new recipes. (You have to finish the first few intro quest at Nazjatar before you can see these NPCs.)
+
+N 145 - 150|N|5 x Rank 1 - Notorious Combatant's Sorcerous Scepter - 5 Star Wood, 125 Gloom Dust, 35 Umbra Shard, 15 Expulsom. The Star Wood is sold by Enchanting supply vendors, trade good vendors, or from the Enchanting Trainer. The closest one I think is back at your trainer.
+
+N You need a lot of Expulsom to reach 175, if you don't know how to get this item then scrapping is probably the quickest way
+
+N Buy the rank 2 Formula: Notorious Combatant's Sorcerous Scepter recipe for 2 Mark of Honor from Dazzerian at Nazjatar.
+N Buy the rank 2 Formula: Notorious Combatant's Sorcerous Scepter recipe for 2 Mark of Honor from Crafticus Mindbender at Nazjatar.
+
+N150 - 165|N| 15 x Rank 2 - Notorious Combatant's Sorcerous Scepter - 15 Star Wood, 345 Gloom Dust, 90 Umbra Shard, 30 Expulsom..Switch to Light-Step Hoofplates between 155-170 if you can get cheap Monel-Hardened Hoofplates.
+
+N Buy the Rank 3 Formula: Notorious Combatant's Sorcerous Scepter recipe for 4 Mark of Honor from the same NPCs as the rank 2. 
+
+N 165 - 175|N| 10 x Rank 3 - Notorious Combatant's Sorcerous Scepter - 10 Star Wood, 200 Gloom Dust, 50 Umbra Shard, 10 Expulsom
+
+N Congratulations on reaching 175!
+
+]]
+
+end)
