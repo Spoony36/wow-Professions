dc514b84bd608ece765de870df9294dfac68a29a
Spoony36 <Spoony36@users.noreply.github.com>
2020-04-18 20:52:36 +0100

    Add files via upload

--- BFA Tailoring.lua
+++ BFA Tailoring.lua
@@ -10,16 +10,16 @@ N Tailoring Trainers:|N|You can find Daniel Brineweaver is in Boralus at the Tra
 N Recipe Ranks|N|Most high-level Battle for Azeroth Tailoring recipes has 3 ranks\nHigher rank recipes reduce the materials required to create a recipe, and also give skill points much longer.
 N Approximate Materials Required|N|590 Tidespray Linen\n280 Deep Sea Satin\n1175 Nylon Thread\n215 Gilded Seaweave\n45 Expulsom\nThe Nylon Thread is sold by your trainer or the tailoring supply vendor near your trainer.
 
-L 1 - 60|N|100 x Tidespray Linen Bandage. The recipe will be yellow/green for the last points, so you probably have to make a few more\n
-L 60 - 75|N|15 x Tidespray Linen Robe. The Nylon Thread is sold by your trainer or the tailoring supply vendor near your trainer\n
-L 75 - 150|N|150 x Embroidered Deep Sea Satin\nYou will use every Embroidered Deep Sea Satin when you craft your epic armors and later when you upgrade them Or you will use them when you get the Pattern: Embroidered Deep Sea Bag recipe\nRandom world drop from mobs at Kul Tiras / Zandalar\nThe recipe will be yellow/green for the last points, so you probably have to make a few more.
+N 1-60|P|Tailoring;197;7+60;1|N|100 x Tidespray Linen Bandage. The recipe will be yellow/green for the last points, so you probably have to make a few more\n
+N 60-75|P|Tailoring;197;7+75;1|N|15 x Tidespray Linen Robe. The Nylon Thread is sold by your trainer or the tailoring supply vendor near your trainer\n
+N 75-150|P|Tailoring;197;7+150;1|N|150 x Embroidered Deep Sea Satin\nYou will use every Embroidered Deep Sea Satin when you craft your epic armors and later when you upgrade them Or you will use them when you get the Pattern: Embroidered Deep Sea Bag recipe\nRandom world drop from mobs at Kul Tiras / Zandalar\nThe recipe will be yellow/green for the last points, so you probably have to make a few more.
 N Visit your trainer Jada (Horde) / Instructor Alikana (Alliance) at Nazjatar and learn the new recipes. (You have to finish the first few intro quest at Nazjatar before you can see these NPCs.)
-L 150 - 160|N|10 x Rank 1 - Notorious Combatant's Satin Bracers.
+N 150-160|P|Tailoring;197;7+160;1|N|10 x Rank 1 - Notorious Combatant's Satin Bracers.
 N Buy the rank 2 Recipe: Notorious Combatant's Satin Bracers recipe for 2 Mark of Honor from Dazzerian at Nazjatar.|FACTION|Horde|
 N Buy the rank 2 Recipe: Notorious Combatant's Satin Bracers recipe for 2 Mark of Honor from Crafticus Mindbender at Nazjatar.|FACTION|Alliance|
-L 160 - 170|N|10 x Rank 2 - Notorious Combatant's Satin Bracers.You should stop at 165 if you want to craft the new epic cloth armors since you will get 12 skill points in total for crafting the 3 gloves and 3 pants.
-Buy the rank 3 Recipe: Notorious Combatant's Satin Bracers recipe for 4 Mark of Honor from the same NPCs as the rank 2. 
-L 170 - 175|N|5 x Rank 3 - Notorious Combatant's Satin Bracers
+N 160-170|P|Tailoring;197;7+170;1|N|10 x Rank 2 - Notorious Combatant's Satin Bracers.You should stop at 165 if you want to craft the new epic cloth armors since you will get 12 skill points in total for crafting the 3 gloves and 3 pants.
+N Buy the rank 3 Recipe: Notorious Combatant's Satin Bracers recipe for 4 Mark of Honor from the same NPCs as the rank 2. 
+N 170-175|P|Tailoring;197;7+175;1|N|5 x Rank 3 - Notorious Combatant's Satin Bracers
 N I hope you liked this leveling guide, congratulations on reaching 175!
 ]]
 
