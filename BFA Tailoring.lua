325189215a4f4d6fc8e5e9b0e4becee37a073c20
Spoony36 <Spoony36@users.noreply.github.com>
2020-04-18 18:55:19 +0100

    Add files via upload

--- BFA Tailoring.lua
+++ BFA Tailoring.lua
@@ -4,15 +4,15 @@ WoWPro:GuideLevels(guide)
 WoWPro:GuideName(guide, "BFA Tailoring")
 WoWPro:GuideSteps(guide, function()
 return [[
-N Disclaimer|N| We are not saying you have to follow this guide, we have provided it as a base guideline only so any errors/suggestions please post in the discord group as normal. You may have a better way of levelling your profession, if so we welcome suggestions on edits that can be made.
-N Tailoring Trainers:|N|You can find Pin'jin the Patient in Dazar'alor at the Terrace of Crafters.You can walk up to a guard in Dazar'alor or Boralus then ask where the Tailoring trainer is. Asking the guard will place a red marker on your map at the trainer's location.
-N Tailoring Trainers:|N|You can find Daniel Brineweaver is in Boralus at the Tradewinds Market.You can walk up to a guard in Dazar'alor or Boralus then ask where the Tailoring trainer is. Asking the guard will place a red marker on your map at the trainer's location.
-N Recipe Ranks|N|Most high-level Battle for Azeroth Tailoring recipes has 3 ranks. Higher rank recipes reduce the materials required to create a recipe, and also give skill points much longer.
-N Approximate Materials Required|N|590 Tidespray Linen,280 Deep Sea Satin,1175 Nylon Thread,215 Gilded Seaweave,45 Expulsom. The Nylon Thread is sold by your trainer or the tailoring supply vendor near your trainer.
+N Disclaimer|N| We are not saying you have to follow this guide\nWe have provided it as a base guideline only so any errors/suggestions please post in the discord group as normal\nYou may have a better way of levelling your profession, if so we welcome suggestions on edits that can be made\n
+N Tailoring Trainers:|N|You can find Pin'jin the Patient in Dazar'alor at the Terrace of Crafters.You can walk up to a guard in Dazar'alor or Boralus then ask where the Tailoring trainer is. Asking the guard will place a red marker on your map at the trainer's location.|FACTION|Horde|
+N Tailoring Trainers:|N|You can find Daniel Brineweaver is in Boralus at the Tradewinds Market.You can walk up to a guard in Dazar'alor or Boralus then ask where the Tailoring trainer is. Asking the guard will place a red marker on your map at the trainer's location.|FACTION|Alliance|
+N Recipe Ranks|N|Most high-level Battle for Azeroth Tailoring recipes has 3 ranks\nHigher rank recipes reduce the materials required to create a recipe, and also give skill points much longer.
+N Approximate Materials Required|N|590 Tidespray Linen\n280 Deep Sea Satin\n1175 Nylon Thread\n215 Gilded Seaweave\n45 Expulsom\nThe Nylon Thread is sold by your trainer or the tailoring supply vendor near your trainer.
 
-L 1 - 60|N|100 x Tidespray Linen Bandage. The recipe will be yellow/green for the last points, so you probably have to make a few more. 
-L 60 - 75|N|15 x Tidespray Linen Robe. The Nylon Thread is sold by your trainer or the tailoring supply vendor near your trainer.
-L 75 - 150|N|150 x Embroidered Deep Sea Satin...You will use every Embroidered Deep Sea Satin when you craft your epic armors and later when you upgrade them. Or you will use them when you get the Pattern: Embroidered Deep Sea Bag recipe. (Random world drop from mobs at Kul Tiras / Zandalar).The recipe will be yellow/green for the last points, so you probably have to make a few more.
+L 1 - 60|N|100 x Tidespray Linen Bandage. The recipe will be yellow/green for the last points, so you probably have to make a few more\n
+L 60 - 75|N|15 x Tidespray Linen Robe. The Nylon Thread is sold by your trainer or the tailoring supply vendor near your trainer\n
+L 75 - 150|N|150 x Embroidered Deep Sea Satin\nYou will use every Embroidered Deep Sea Satin when you craft your epic armors and later when you upgrade them Or you will use them when you get the Pattern: Embroidered Deep Sea Bag recipe\nRandom world drop from mobs at Kul Tiras / Zandalar\nThe recipe will be yellow/green for the last points, so you probably have to make a few more.
 N Visit your trainer Jada (Horde) / Instructor Alikana (Alliance) at Nazjatar and learn the new recipes. (You have to finish the first few intro quest at Nazjatar before you can see these NPCs.)
 L 150 - 160|N|10 x Rank 1 - Notorious Combatant's Satin Bracers.
 N Buy the rank 2 Recipe: Notorious Combatant's Satin Bracers recipe for 2 Mark of Honor from Dazzerian at Nazjatar.|FACTION|Horde|
