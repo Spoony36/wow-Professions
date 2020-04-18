5901ae8a13d6e289c3a175795b3ee6375a8e07e2
Spoony36 <Spoony36@users.noreply.github.com>
2020-04-18 20:26:35 +0100

    Add files via upload

--- BFA Blacksmithing.lua
+++ BFA Blacksmithing.lua
@@ -1,5 +1,5 @@
 local guide = WoWPro:RegisterGuide("BFA Blacksmithing","Profession","BFA Blacksmithing", "Spoony", "Neutral")
-WoWPro:GuideIcon(guide,"PRO",171)
+WoWPro:GuideIcon(guide,"PRO",164)
 WoWPro:GuideName(guide, "BFA Blacksmithing")
 WoWPro:GuideLevels(guide)
 WoWPro:GuideSteps(guide, function()
@@ -9,38 +9,38 @@ N Blacksmithing Trainers:|N|You can find Forgemaster Zak'aal in Dazar'alor at th
 N Blacksmithing Trainers:|M|73.40,8.40|Z|Boralus|N|You can find Grix "Ironfists" Barlow in Boralus at the Tradewinds Market\nYou can walk up to a guard in Boralus then ask where the Blacksmithing trainer is\nAsking the guard will place a red marker on your map at the trainer's location.|FACTION|Alliance|
 N Recipe Ranks|N| Most high-level Battle for Azeroth Blacksmithing recipes has 3 ranks\nHigher rank recipes reduce the materials required to create a recipe, and also give skill points much longer.
 N Note|N| You can buy Durable Flux from the Blacksmithing trainer\nI didn't list them in the guide since you can buy a bunch of them from the vendor\n
-N Levelling from 1 - 15|N|Craft 15 x Monel-Hardened Armguards\nYou might need to make a few more.
-N Levelling from 15 - 28|N|Craft 13 x Monel-Hardened Waistguard\n
-N Levelling from 28 - 35|N|Craft 7 x Monel-Hardened Greaves\n
-N Levelling from 35 - 50|N|Craft 15 x Monel-Hardened Breastplate\n
-N Levelling from 50 - 60|N|Craft 20 x Monelite Skeleton Key\nYou might need to make a few more\n
-N Levelling from 60 - 85|N|Craft 25 x Monel-Hardened Breastplate\nYou will probably need to make a few more\n
-N Levelling from 85 - 95|N|Craft 10 x Rank 1 - Honorable Combatant's Plate Armguards\n
+N Levelling from 1-15|P|Blacksmithing;164;7+15;1|N|Craft 15 x Monel-Hardened Armguards\nYou might need to make a few more.
+N Levelling from 15-28|P|Blacksmithing;164;7+28;1|N|Craft 13 x Monel-Hardened Waistguard\n
+N Levelling from 28-35|P|Blacksmithing;164;7+35;1|N|Craft 7 x Monel-Hardened Greaves\n
+N Levelling from 35-50|P|Blacksmithing;164;7+50;1|N|Craft 15 x Monel-Hardened Breastplate\n
+N Levelling from 50-60|P|Blacksmithing;164;7+60;1|N|Craft 20 x Monelite Skeleton Key\nYou might need to make a few more\n
+N Levelling from 60-85|P|Blacksmithing;164;7+85;1|N|Craft 25 x Monel-Hardened Breastplate\nYou will probably need to make a few more\n
+N Levelling from 85-95|P|Blacksmithing;164;7+95;1|N|Craft 10 x Rank 1 - Honorable Combatant's Plate Armguards\n
 N Action|N|If you have a lot of Mark of Honor, buy the rank 2 and rank 3 from Ozgrom Ragefang in Zuldazar\nYou will only need 200 Storm Silver Ore if you buy the rank 3 recipes\nBut only buy it if you have more than 18 Mark of Honor\nYou should save 12 Mark of Honor for the last two recipe\n|FACTION|Horde|
 N Action|N|If you have a lot of Mark of Honor, buy the rank 2 and rank 3 from Leedan Gustaf in Boralus\nYou will only need 200 Storm Silver Ore if you buy the rank 3 recipes\nBut only buy it if you have more than 18 Mark of Honor\nYou should save 12 Mark of Honor for the last two recipe\n|FACTION|Alliance|
 N Note|N| You can buy Elemental Flux from the Blacksmithing trainer\nI didn't list them in the guide since you can just buy a bunch of them from the vendor.
 N Action|N|Learn Rank 2 - Monel-Hardened Hoofplates from your trainer\n
 N Action|N|Go somewhere in Zandalar or Kul Tiras where you can find a lot of HUMANOID mobs\nKill those mobs until you get the Rank 3 - Recipe: Monel-Hardened Hoofplates\nThe drop rate is high\nYou should get this recipe after 10-20 kills(drops only from Humanoid mobs)\nNote: Make sure you don't already have the recipe in your bags/bank, because it won't drop twice\n
-N Levelling from 95 - 115|N|Craft 20 x Rank 3 - Monel-Hardened Hoofplates
-N Levelling from 115 - 145|N|The materials required for this part will depend on if you have any Mark of Honor or if you are willing to do PvP to get some\nIf you don't have any Mark of Honor, but you want to reach 145, your best option is to wait until you get to revered with The Honorbound then you can buy the rank 3 Stormsteel recipes\nI understand that a lot of players hate PvP, but I highly recommend to farm the 6 Mark of Honor\nIt's a lot cheaper to level Blacksmithing with Sinister/Notorious Combatant Combatant weapons and armors\nHow to get Mark of Honor?. You can get Mark of Honor from doing Battlegrounds and Arenas. The most reliable way is to just do some Battlegrounds since you get Mark of Honor most of the time\nYou will get 2 for a win, and 1 for a loss but only if you don't lose with very low score (like 100-200 score, or 0 flags)\n.|FACTION|Horde|
+N Levelling from 95-115|P|Blacksmithing;164;7+115;1|N|Craft 20 x Rank 3 - Monel-Hardened Hoofplates
+N Levelling from 115-145|P|Blacksmithing;164;7+145;1|N|The materials required for this part will depend on if you have any Mark of Honor or if you are willing to do PvP to get some\nIf you don't have any Mark of Honor, but you want to reach 145, your best option is to wait until you get to revered with The Honorbound then you can buy the rank 3 Stormsteel recipes\nI understand that a lot of players hate PvP, but I highly recommend to farm the 6 Mark of Honor\nIt's a lot cheaper to level Blacksmithing with Sinister/Notorious Combatant Combatant weapons and armors\nHow to get Mark of Honor?. You can get Mark of Honor from doing Battlegrounds and Arenas. The most reliable way is to just do some Battlegrounds since you get Mark of Honor most of the time\nYou will get 2 for a win, and 1 for a loss but only if you don't lose with very low score (like 100-200 score, or 0 flags)\n.|FACTION|Horde|
 N Action|N|Buy the rank 2 and rank 3 Recipe: Sinister Combatant's Plate Armguards recipe for 6 Mark of Honor from Ozgrom Ragefang in Zuldazar.|FACTION|Horde|
-N Levelling from 115 - 145|N|The materials required for this part will depend on if you have any Mark of Honor or if you are willing to do PvP to get some\nIf you don't have any Mark of Honor, but you want to reach 145, your best option is to wait until you get to revered with 7th Legion then you can buy the rank 3 Stormsteel recipes\nI understand that a lot of players hate PvP, but I highly recommend to farm the 6 Mark of Honor\nIt's a lot cheaper to level Blacksmithing with Sinister/Notorious Combatant Combatant weapons and armors\nHow to get Mark of Honor?. You can get Mark of Honor from doing Battlegrounds and Arenas. The most reliable way is to just do some Battlegrounds since you get Mark of Honor most of the time\nYou will get 2 for a win, and 1 for a loss but only if you don't lose with very low score (like 100-200 score, or 0 flags)\n.|FACTION|Alliance|
+N Levelling from 115-145|P|Blacksmithing;164;7+145;1|N|The materials required for this part will depend on if you have any Mark of Honor or if you are willing to do PvP to get some\nIf you don't have any Mark of Honor, but you want to reach 145, your best option is to wait until you get to revered with 7th Legion then you can buy the rank 3 Stormsteel recipes\nI understand that a lot of players hate PvP, but I highly recommend to farm the 6 Mark of Honor\nIt's a lot cheaper to level Blacksmithing with Sinister/Notorious Combatant Combatant weapons and armors\nHow to get Mark of Honor?. You can get Mark of Honor from doing Battlegrounds and Arenas. The most reliable way is to just do some Battlegrounds since you get Mark of Honor most of the time\nYou will get 2 for a win, and 1 for a loss but only if you don't lose with very low score (like 100-200 score, or 0 flags)\n.|FACTION|Alliance|
 N Action|N|Buy the rank 2 and rank 3 Recipe: Sinister Combatant's Plate Armguards recipe for 6 Mark of Honor from Leedan Gustaf in Boralus.|FACTION|Alliance|
-N Levelling from 115 - 145|N|Craft 30 x Rank 3 - Sinister Combatant's Plate Armguards
-N Levelling from 115-120|N| If you don't have any Mark of Honor, Craft 5 x Rank 1 - Honorable Combatant's Plate Greaves\nThe recipe is sold by Ransa Greyfeather and It requires Revered with The Honorbound\nYou will probably have to craft a lot more since the recipe will be green for the last few points.|FACTION|Horde|
-N Levelling from 120-145|N| Craft 40 x Rank 3 - Stormsteel Dagger\nThe recipe is sold by Ransa Greyfeather and It requires Revered with The Honorbound\nYou will probably have to craft a lot more since the recipe will be green for the last few points.|FACTION|Horde|
-N Levelling from 115-120|N| If you don't have any Mark of Honor, Craft 5 x Rank 1 - Honorable Combatant's Plate Greaves\nThe recipe is sold by Vindicator Jaelaana and it requires Revered with The 7th Legion\nYou will probably have to craft a lot more since the recipe will be green for the last few points.|FACTION|Alliance|
-N Levelling from 120-145|N| Craft 40 x Rank 3 - Stormsteel Dagger\nThe recipe is sold by Vindicator Jaelaana and it requires Revered with The 7th Legion\nYou will probably have to craft a lot more since the recipe will be green for the last few points.|FACTION|Alliance|
-N Levelling from 145 - 160|N| Visit your trainer Jada at Nazjatar and learn the new recipes\nYou have to finish the first few intro quest at Nazjatar before you can see these NPCs\n|FACTION|Horde|
+N Levelling from 115-145|P|Blacksmithing;164;7+145;1|N|Craft 30 x Rank 3 - Sinister Combatant's Plate Armguards
+N Levelling from 115-120|P|Blacksmithing;164;7+120;1|N| If you don't have any Mark of Honor, Craft 5 x Rank 1 - Honorable Combatant's Plate Greaves\nThe recipe is sold by Ransa Greyfeather and It requires Revered with The Honorbound\nYou will probably have to craft a lot more since the recipe will be green for the last few points.|FACTION|Horde|
+N Levelling from 120-145|P|Blacksmithing;164;7+145;1|N| Craft 40 x Rank 3 - Stormsteel Dagger\nThe recipe is sold by Ransa Greyfeather and It requires Revered with The Honorbound\nYou will probably have to craft a lot more since the recipe will be green for the last few points.|FACTION|Horde|
+N Levelling from 115-120|P|Blacksmithing;164;7+120;1|N| If you don't have any Mark of Honor, Craft 5 x Rank 1 - Honorable Combatant's Plate Greaves\nThe recipe is sold by Vindicator Jaelaana and it requires Revered with The 7th Legion\nYou will probably have to craft a lot more since the recipe will be green for the last few points.|FACTION|Alliance|
+N Levelling from 120-145|P|Blacksmithing;164;7+145;1|N| Craft 40 x Rank 3 - Stormsteel Dagger\nThe recipe is sold by Vindicator Jaelaana and it requires Revered with The 7th Legion\nYou will probably have to craft a lot more since the recipe will be green for the last few points.|FACTION|Alliance|
+N Levelling from 145-160|P|Blacksmithing;164;7+160;1|N| Visit your trainer Jada at Nazjatar and learn the new recipes\nYou have to finish the first few intro quest at Nazjatar before you can see these NPCs\n|FACTION|Horde|
 N Craft|N|Craft 15 x Rank 1 - Notorious Combatant's Plate Armguards|FACTION|Horde|
-N Levelling from 145 - 160|N| Visit your trainer Instructor Alikana at Nazjatar and learn the new recipes\nYou have to finish the first few intro quest at Nazjatar before you can see these NPCs\n|FACTION|Alliance|
+N Levelling from 145-160||P|Blacksmithing;164;7+160;1N| Visit your trainer Instructor Alikana at Nazjatar and learn the new recipes\nYou have to finish the first few intro quest at Nazjatar before you can see these NPCs\n|FACTION|Alliance|
 N Craft|N|Craft 15 x Rank 1 - Notorious Combatant's Plate Armguards|FACTION|Alliance|
-N Levelling from 160 - 170|N|You should stop at 165 if you plan to craft the new epic plate armors since you will get 12 skill points in total for crafting the 3 belt and 3 pants\n
+N Levelling from 160-170|P|Blacksmithing;164;7+170;1|N|You should stop at 165 if you plan to craft the new epic plate armors since you will get 12 skill points in total for crafting the 3 belt and 3 pants\n
 N Action|N|Buy the Rank 2 - Recipe: Notorious Combatant's Plate Armguards recipe for 2 Mark of Honor from Dazzerian at Nazjatar\n|FACTION|Horde|
 N Action|N|Buy the Rank 2 - Recipe: Notorious Combatant's Plate Armguards recipe for 2 Mark of Honor from Crafticus Mindbender at Nazjatar\n|FACTION|Alliance|
 N Action|N|Craft 10 x rank 2 - Notorious Combatant's Plate Armguards
 N Action|N|Buy the Rank 3 - Recipe: Notorious Combatant's Plate Armguards recipe for 4 Mark of Honor from the same NPCs as the rank 2\n
-N Levelling from 170 - 175|N|Craft 5 x Rank 3 - Notorious Combatant's Plate Armguards
+N Levelling from 170-175|P|Blacksmithing;164;7+175;1|N|Craft 5 x Rank 3 - Notorious Combatant's Plate Armguards
 N Congratulations on reaching 175
 ]]
 end)
--- BFA Cooking.lua
+++ BFA Cooking.lua
@@ -1,5 +1,5 @@
 local guide = WoWPro:RegisterGuide("BFA Cooking","Profession","BFA Cooking", "Spoony", "Neutral")
-WoWPro:GuideIcon(guide,"PRO",171)
+WoWPro:GuideIcon(guide,"PRO",185)
 WoWPro:GuideName(guide, "BFA Cooking")
 WoWPro:GuideLevels(guide)
 WoWPro:GuideSteps(guide, function()
@@ -15,19 +15,19 @@ N Cooking Reagents|N|The following Cooking reagents are all sold by Henrick Wyth
 N Extracting Oil|N| You can extract Aromatic Fish Oil from any Battle for Azeroth fish\nYou have to right-click the fish you caught, and it will convert the fish into oil.
 N Fish|N| Fishes no longer come from one specific zone\nInstead, you can find 3 types of fish in the Kul Tiras zones\n3 in the Zandalar zones, 2 in Nazjatar, 1 in Mechagon and you can fish 2 in all zones.
 N Fish|N|Mechagon:Ionized Minnow\nNazjatar:Viper Fish,Mauve Stinger\nKul Tiras:Frenzied Fangtooth and Lane Snapper - Fished from seas/oceans\nTiragarde Perch - Fished from rivers/lakes\nZandalar:Slimy Mackerel and Sand Shifter - Fished from seas/oceans & Redtail Loach - Fished from rivers/lakes\nAll zones:Great Sea Catfish - Rivers and lakes(except Nazjatar), Midnight Salmon - Everywhere(rare fish, around 3% catch rate)
-L Levelling from 1 - 35|N| Cook 34 x Mon'Dazi
+N Levelling from 1 - 35|P|Cooking;185;7+35;1|N| Cook 34 x Mon'Dazi
 N Action|N|Learn the Rank 2 recipes from your trainer
 N Note|N|You don't have to make Kul Tiramisu, you can make any other dessert recipe\nCook 15 x Rank 2 - Kul Tiramisu
-L Levelling from 50 - 75|N| Make 25 from one of the these three\n25 x Seasoned Loins\n25 x Honey-Glazed Haunches\n25 x Sailor's Pie\nIf you make Seasoned Loins, don't vendor them! Sometimes you can use them for Warfront Contributions quests when the Warfronts are up.
+N Levelling from 50 - 75|P|Cooking;185;7+75;1|N| Make 25 from one of the these three\n25 x Seasoned Loins\n25 x Honey-Glazed Haunches\n25 x Sailor's Pie\nIf you make Seasoned Loins, don't vendor them! Sometimes you can use them for Warfront Contributions quests when the Warfronts are up.
 N Note|N| These meats are usually really cheap\nI recommend to switch to these dishes at 50, but if they are expensive on your realm, then it's better to make more Rank 2 - Kul Tiramisu up to around 75\nAs a general rule, if 1 Aromatic Fish Oil cost around the same as these meats, then you should make Kul Tiramisu because even if you have to craft 2-3 for a skill point, you would still only need 4-6 Fish Oil, instead of the 10 meats.
 N Learn the Rank 2 recipes from your trainer
-N Action|N|Make around 120 from one of these three\nRank 2 - Seasoned Loins\nRank 2 - Honey-Glazed Haunches\nRank 2 - Sailor's Pie\nThese will be green for the last 10 points, so you might have to make more.
+N Levelling from 75 - 140|P|Cooking;185;7+140;1|N|Make around 120 from one of these three\nRank 2 - Seasoned Loins\nRank 2 - Honey-Glazed Haunches\nRank 2 - Sailor's Pie\nThese will be green for the last 10 points, so you might have to make more.
 N Rank 3 recipes|N|If you can get the rank 3 of these recipes, then you can make them until 150\nRecipe: Honey-Glazed Haunches - Reward from the World Quest: "Work Order: Honey-Glazed Haunches" (this one will show up randomly sometimes on your map)\nRecipe: Seasoned Loins - Requires Tortollan Seekers - Revered. Sold by Collector Kojo\nRecipe: Sailor's Pie - Reward from the World Quest: "Work Order: Sailor's Pie" (this one will show up randomly sometimes on your map)
-L Levelling from 140 - 155|N|Visit your trainer Rolm at Nazjatar and learn the new recipes\nYou have to finish the first few intro quest at Nazjatar before you can see these NPCs|FACTION|Horde|
-L Levelling from 140 - 155|N|Visit your trainer Instructor Ulooaka at Nazjatar and learn the new recipes\nYou have to finish the first few intro quest at Nazjatar before you can see these NPCs|FACTION|Alliance|
-N Levelling from 140 - 155|N|Cook 15 x Rank 1 - Abyssal-Fried Rissole\nYou can make other recipes, but Moist Fillet should be the cheapest
+N Levelling from 140 - 155|N|Visit your trainer Rolm at Nazjatar and learn the new recipes\nYou have to finish the first few intro quest at Nazjatar before you can see these NPCs|FACTION|Horde|
+N Levelling from 140 - 155|N|Visit your trainer Instructor Ulooaka at Nazjatar and learn the new recipes\nYou have to finish the first few intro quest at Nazjatar before you can see these NPCs|FACTION|Alliance|
+N Levelling from 140 - 155|P|Cooking;185;7+155;1|N|Cook 15 x Rank 1 - Abyssal-Fried Rissole\nYou can make other recipes, but Moist Fillet should be the cheapest
 N Learn the rank 2 recipes
-N Levelling from 155 - 175|N|Cook 25 x Rank 2 - Abyssal-Fried Rissole\n
+N Levelling from 155 - 175|P|Cooking;185;7+175;1|N|Cook 25 x Rank 2 - Abyssal-Fried Rissole\n
 N I hope you liked this guide, congratulations on reaching 175!\n
 
 ]]
