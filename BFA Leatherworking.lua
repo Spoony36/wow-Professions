5d9df5288c6548621fa8e7432a54a6aa0b3a650d
Spoony36 <Spoony36@users.noreply.github.com>
2020-04-18 20:38:40 +0100

    Add files via upload

--- BFA Leatherworking.lua
+++ BFA Leatherworking.lua
@@ -1,5 +1,5 @@
 local guide = WoWPro:RegisterGuide("BFA Leatherworking","Profession","BFA Leatherworking", "Spoony", "Neutral")
-WoWPro:GuideIcon(guide,"PRO",164)
+WoWPro:GuideIcon(guide,"PRO",165)
 WoWPro:GuideLevels(guide)
 WoWPro:GuideName(guide, "BFA Leatherworking")
 WoWPro:GuideSteps(guide, function()
@@ -12,18 +12,18 @@ N Leatherworking Trainers|N|You can find Cassandra Brennor is in Boralus at the 
 N Recipe Ranks|N| Most high-level Battle for Azeroth Leatherworking recipes has 3 ranks. Higher rank recipes reduce the materials required to create a recipe, and also give skill points much longer.
 N Approximate Materials Required|N|2025 Coarse Leather, 350 Shimmerscale or an extra 350 Coarse Leather, 50 Tempest Hide, 550 Blood-Stained Bone, 400 Dredged Leather
 
-L 1 - 25|N| Make 25 from one of these: 25 x Shimmerscale Armguards or 25 x Coarse Leather Armguards. You might need to make a few more to reach 25.
-L 25 - 50|N| Make 25 from one of these: 25 x Shimmerscale Pauldrons or 25 x Coarse Leather Pauldrons. 
-L 50 - 70. Hardened Tempest Hide is used for crafting some high-level armor. These are basically free skill ups, but even if you didn't use it for anything, this is still the cheapest recipe. Tempest Hide is easy to farm.|N|50 x Hardened Tempest Hide. You can buy the Amber Tanning Oil from Aman de Child. She is like 30 yard in front of your trainer.This recipe will be green for the last 5 points so you might have to make more. (or fewer if you are lucky with the skill gains)|FACTION|Horde|
-L 50 - 70. Hardened Tempest Hide is used for crafting some high-level armor. These are basically free skill ups, but even if you didn't use it for anything, this is still the cheapest recipe. Tempest Hide is easy to farm.|N|50 x Hardened Tempest Hide. You can buy the Amber Tanning Oil from the Leatherworking trainer.This recipe will be green for the last 5 points so you might have to make more. (or fewer if you are lucky with the skill gains)|FACTION|Alliance|
-L 70 - 85|N| Make 15 x Coarse Leather Barding. Making Shimmerscale Diving Helmet would be cheaper, but the Coarse Leather Barding is much more useful and easier to sell.
+N 1-25|P|Leatherworking;165;7+25;1|N| Make 25 from one of these\n25 x Shimmerscale Armguards\n25 x Coarse Leather Armguards.\nYou might need to make a few more to reach 25.
+N 25-50|P|Leatherworking;165;7+50;1|N| Make 25 from one of these\n25 x Shimmerscale Pauldrons\n25 x Coarse Leather Pauldrons. 
+N 50-70|P|Leatherworking;165;7+70;1|N|50 x Hardened Tempest Hide. You can buy the Amber Tanning Oil from Aman de Child. She is like 30 yard in front of your trainer.This recipe will be green for the last 5 points so you might have to make more. (or fewer if you are lucky with the skill gains)|FACTION|Horde|
+N 50-70|P|Leatherworking;165;7+70;1|N|50 x Hardened Tempest Hide. You can buy the Amber Tanning Oil from the Leatherworking trainer.This recipe will be green for the last 5 points so you might have to make more. (or fewer if you are lucky with the skill gains)|FACTION|Alliance|
+N 70-85|N| Make 15 x Coarse Leather Barding. Making Shimmerscale Diving Helmet would be cheaper, but the Coarse Leather Barding is much more useful and easier to sell.
 N Learn the rank 2 recipe for Coarse Leather Barding from your trainer!
-L 85 - 120|N| Make 45 x Rank 2 - Coarse Leather Barding. If you have the Rank 3 - Recipe: Coarse Leather Barding, then you can make this one up to 125, and it will cost fewer Coarse Leathers. The recipe is a random drop from humanoid mobs in Battle for Azeroth zones.
-L 120 - 150|N| Make 30 x Drums of the Maelstrom
+N 85-120|P|Leatherworking;165;7+120;1|N| Make 45 x Rank 2 - Coarse Leather Barding. If you have the Rank 3 - Recipe: Coarse Leather Barding, then you can make this one up to 125, and it will cost fewer Coarse Leathers. The recipe is a random drop from humanoid mobs in Battle for Azeroth zones.
+N 120-150|P|Leatherworking;165;7+150;1|N| Make 30 x Drums of the Maelstrom
 N Visit your trainer Jada in Nazjatar and learn the new recipes. (You have to finish the first few intro quest at Nazjatar before you can see these NPCs.)|FACTION|Horde|
 N Visit your trainer Instructor Alikanaat in Nazjatar and learn the new recipes. (You have to finish the first few intro quest at Nazjatar before you can see these NPCs.)|FACTION|Alliance|
-L 150 - 175|N| Make 40 x Dredged Leather Bladder. Alternative recipe, If you have 6 Mark of Honor and a bunch of Expulsom that you don't use, then Notorious Combatant's Mail Armguards is a good alternative. The rank 2 and 3 are sold by Dazzerian in Nazjatar. You need Leatherworking 160 for the rank 2 and 170 for the rank 3.|FACTION|Horde|
-L 150 - 175|N| Make 40 x Dredged Leather Bladder. Alternative recipe, If you have 6 Mark of Honor and a bunch of Expulsom that you don't use, then Notorious Combatant's Mail Armguards is a good alternative. The rank 2 and 3 are sold by Crafticus Mindbender in Nazjatar. You need Leatherworking 160 for the rank 2 and 170 for the rank 3.|FACTION|Alliance|
+N 150-175|P|Leatherworking;165;7+175;1|N| Make 40 x Dredged Leather Bladder. Alternative recipe, If you have 6 Mark of Honor and a bunch of Expulsom that you don't use, then Notorious Combatant's Mail Armguards is a good alternative. The rank 2 and 3 are sold by Dazzerian in Nazjatar. You need Leatherworking 160 for the rank 2 and 170 for the rank 3.|FACTION|Horde|
+N 150-175|P|Leatherworking;165;7+175;1|N| Make 40 x Dredged Leather Bladder. Alternative recipe, If you have 6 Mark of Honor and a bunch of Expulsom that you don't use, then Notorious Combatant's Mail Armguards is a good alternative. The rank 2 and 3 are sold by Crafticus Mindbender in Nazjatar. You need Leatherworking 160 for the rank 2 and 170 for the rank 3.|FACTION|Alliance|
 N I hope you liked this leveling guide, congratulations on reaching 175!
 ]]
 
